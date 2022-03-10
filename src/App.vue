3dccfe758a9ce69522aa776cec260fd0e27341ba
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-03-10 04:16:46 +0300

    Navigation Bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 
 <script setup>
-	import LeftSideNavigation from './components/LeftSideNavigation.vue';
+	import TheNavigation from './components/TheNavigation.vue';
 	import TimeLine from './components/TimeLine.vue';
 	import Trends from './components/Trends.vue';
 </script>
 
 <template>
 	<div class="h-auto w-[79.063rem] m-auto flex relative">
-	   <LeftSideNavigation/>
+	   <TheNavigation/>
 	   <TimeLine/>	  
 	   <Trends class="w-[24.375rem] h-auto min-h-screen max-h-auto  relative bg-black pl-[1.875rem] pt-[4.063rem]"/>
 	</div>
