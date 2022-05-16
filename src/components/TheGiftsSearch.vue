51f176cb2d800112143eb71470ec749eeef29ce1
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-05-16 20:55:31 +0300

    Cross Icon

--- src/components/TheGiftsSearch.vue
+++ src/components/TheGiftsSearch.vue
@@ -1,9 +1,6 @@
 <template>
 	<div class="w-full h-12 px-3.5 bg-[#ffffffa6] backdrop-blur-md inline-flex items-center justify-between sticky top-0 z-20">
-		<!--<div class="w-[30px] h-[30px]">
-			<span class="text-white">&#10005</span>
-		</div>-->
-		<Cross :size="0.719" class="w-[0.719rem] h-[0.719rem] fill-[#0f1419]"/>
+		<Cross :size="18" class="w-[1.875rem] h-[1.875rem] hover:bg-[#0f14191a] fill-[696c70]"/>
 		<div class="w-[501px] h-9 bg-white rounded-full relative inline-flex items-center cursor-text" @click="$emit('active', true)" 
 		v-on:click.stop :class="[!searchArea.active ? 'justify-center border border-[#cfd9de]': 'border-2 border-[#1d9bf0]']">
 			<div class="w-[25px] h-full flexCenter" :class="{'ml-2': searchArea.active}">
@@ -14,9 +11,7 @@
 		    </div> 
 		    <input type="text" class="w-[420px] text-[13px] bg-transparent outline-none"  v-if="searchArea.active" placeholder="Search for GIFs" v-model="search"/>
 			<div class="w-9 h-full flexCenter justify-start absolute right-1" v-if="searchArea.active && search">
-				<div class="w-[19px] h-[19px] rounded-full bg-black flexCenter cursor-pointer" @click="celarSearch">
-					<span class="text-white text-[10px]">&#10005</span>
-				</div>
+				<Cross :size="11" class="w-[1.219rem] h-[1.219rem]  bg-black fill-white"/>
 		    </div>
 		</div>
 	</div>
