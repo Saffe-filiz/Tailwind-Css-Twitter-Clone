fc90887c172dc8ed1f8f373502014acd256d7371
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-05-18 21:37:35 +0300

    Gif Search Label

--- src/components/TheGiftsSearch.vue
+++ src/components/TheGiftsSearch.vue
@@ -1,10 +1,10 @@
 <template>
-	<div class="w-full h-12 px-3.5 bg-[#ffffffa6] backdrop-blur-md inline-flex items-center justify-between sticky top-0 z-20">
+	<label for="gifSearch"  class="w-full h-12 px-3.5 bg-[#ffffffa6] backdrop-blur-md inline-flex items-center justify-between sticky top-0 z-20">
 		<div class="w-[1.875rem] h-[1.875rem] hover:bg-[#0f14191a] rounded-full flexCenter">
 			<Cross v-if="!search" :size="18" class="fill-[696c70]"/>
-		    <Arrow v-else :size="18" @click="celarSearch"/>
+		    <Arrow v-else :size="18" class="cursor-pointer" @click="celarSearch"/>
 		</div>
-		<label for="gifSearch" class="w-[501px] h-9 bg-white rounded-full relative inline-flex items-center cursor-text" @click="$emit('active', true)" 
+		<div class="w-[501px] h-9 bg-white rounded-full relative inline-flex items-center cursor-text" @click="$emit('active', true)" 
 		v-on:click.stop :class="[!searchArea.active ? 'justify-center border border-[#cfd9de]': 'border-2 border-[#1d9bf0]']">
 			<Search :size="14"  class="flexCenter fill-[#536471]" :class="{'ml-[11px]':  searchArea.active}"/>
 		    <div class="w-auto h-3.5 flexCenter" v-show="!searchArea.active">
@@ -14,8 +14,8 @@
 			<div class="w-9 h-full flexCenter justify-start absolute right-1" v-if="searchArea.active && search">
 			    <Cross :size="11" class="w-[1.219rem] h-[1.219rem]  bg-black fill-white" @click="celarSearch"/>
 		    </div>
-		</label>
-	</div>
+		</div>
+	</label>
 </template>
 
 <script setup>
@@ -35,7 +35,8 @@
     const inputPlaceHolder = computed(() => search.value == '' ? 'Search for GIFs' : search.value);
 
     const searchGif = () =>  store.dispatch('getGifts', search)
-    onMounted(() => document.querySelector('#gifSearch').focus())
+    onMounted(() => inputFocus())
+    const inputFocus = () => document.querySelector('#gifSearch').focus()
 
 
 </script>
