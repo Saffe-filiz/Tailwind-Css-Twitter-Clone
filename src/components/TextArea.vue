0ca8cc7d3dcff165e167a766a08c5de7d94e5b89
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-06-17 16:27:19 +0300

    Save Tweet Modal

--- src/components/TextArea.vue
+++ src/components/TextArea.vue
@@ -1,16 +1,28 @@
 <template>
 	<article class="w-full min-h-12 py-2">
 		<div class="w-full relative">
-		    <div id="textArea" class="w-full outline-none" contenteditable 
-		    @input="$emit('post', $event.target.innerText), showPlaceholderMassage = false"></div>
+		    <div id="textArea" class="w-full outline-none" contenteditable @input="e => tweet = e.target.innerText"></div>
 		    <div class="absolute top-0 left-0 pointer-events-none" v-if="showPlaceholderMassage">{{placeholderMassage}}</div>
 	    </div>
    </article>
 </template>
 
 <script setup>
-	import { ref } from 'vue';
+	import { ref, watch, inject, defineEmits } from 'vue';
+
+	const emit = defineEmits(['post'])
+
+	const modal = inject('modal'); 
+
+	let tweet = ref('') 
+
 
 	let placeholderMassage = ref('What\'s happening ?')
-	let showPlaceholderMassage = ref(true)
+	let showPlaceholderMassage = ref(true);
+
+	watch( tweet, (text) => {
+		modal.openNewTweetModal ? modal.textAreaIsEmty = text != '': null
+		showPlaceholderMassage.value = false;
+		emit('post', text)
+	})
 </script>
