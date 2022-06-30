36aef7680d6e64161abda5047a1b1e9a470c73e0
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-06-30 16:00:22 +0300

    Gif Search Inpıt

--- src/components/Gif/TheGifSearch.vue
+++ src/components/Gif/TheGifSearch.vue
@@ -1,18 +1,37 @@
 <template>
 	<div class="w-full h-12 px-2 bg-[#ffffffa6] backdrop-blur-md inline-flex items-center justify-between sticky top-0 z-20" >
 		<div class="w-[1.875rem] h-[1.875rem] hover:bg-[#0f14191a] rounded-full flexCenter">
-			<CrossIcon v-if="!search" :size="18" class="fill-[696c70]" @click="modal.scrollVisibil, modal.closePopUp()"/>
+			<CrossIcon 
+			    class="fill-[696c70]"
+			    v-if="!search" 
+			    :size="18" 
+			    @click="modal.scrollVisibil, modal.closePopUp()"/>
 		    <ArrowIcon v-else :size="18" class="cursor-pointer" @click="celarSearch"/>
 		</div>
 		<div class="group" @click="inputFocus">
-		    <label for="gifSearch" class="w-[501px]  relative h-9 bg-white rounded-full inline-flex items-center cursor-text border border-[#cfd9de] px-3 group-focus-within:border-2 group-focus-within:border-[#1d9bf0] group-focus-within:justify-start justify-center">
-		    	<SearchIcon :size="14" class="flexCenter fill-[#536471] mr-1"/>
-		        <div class="group-focus-within:w-[420px] inline h-full relative overflow-hidden" :class="[search.length ? 'w-auto': 'w-28']">
+		    <label 
+		        for="gifSearch" 
+		        class="w-[501px]  relative h-9 bg-white rounded-full inline-flex items-center cursor-text border border-[#cfd9de] px-3 group-focus-within:border-2 group-focus-within:border-[#1d9bf0] group-focus-within:justify-start justify-center">
+		    	<SearchIcon  
+		    	    class="flexCenter fill-[#536471] mr-1" 
+		    	    :size="14"/>
+		        <div 
+		            class="group-focus-within:w-[420px] inline h-full relative overflow-hidden" 
+		            :class="[search.length ? 'w-auto': 'w-28']">
 		          	<span class="span relative z-[-1]">{{search}}</span>
-		        	<input type="text" class="w-full h-full absolute z-10 left-0 -top-[1px] outline-none gifSearch" @input="searchGif" v-model="search" placeholder="Search for GIFs" />
+		        	<input 
+		        	    class="w-full h-full absolute z-10 left-0 -top-[1px] outline-none gifSearch"
+		        	    type="text" 
+		        	    @input="getGifs" 
+		        	    placeholder="Search for GIFs" 
+		        	    v-model="search"/>
 		        </div>
 		    <div class="w-9 inline-flex justify-end items-center" v-if="search">
-		    <CrossIcon tabindex="-1" :size="11" class="group-focus-within:visible invisible w-[1.219rem] h-[1.219rem] bg-black fill-white" @click="celarSearch"/>
+		    <CrossIcon 
+		        class="group-focus-within:visible invisible w-[1.219rem] h-[1.219rem] bg-black fill-white"
+		        tabindex="-1" 
+		        :size="11" 
+		        @click="celarSearch"/>
 		    </div>
 		</label>
 	</div>
@@ -36,7 +55,7 @@
 	const getGanre = computed(() => store.getters.getGifGanre);
 	const getNumberOfGif = computed(() => store.getters.getNumberOfGif);
 
-	const getGifs = () => store.dispatch('getGifts', search.value)
+	const getGifs = () => store.dispatch('getGifts', search.value);
 
 
     const celarSearch = () =>  {
